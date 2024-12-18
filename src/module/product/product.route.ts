64beef6567f123346de5cfec657ec8e24f760b18
habib ulla <habibulla1278@gmail.com>
2024-12-18 20:56:51 +0600

    correction added

--- src/module/product/product.route.ts
+++ src/module/product/product.route.ts
@@ -3,7 +3,7 @@ import { productControllers } from './product.controller';
 
 const router = express.Router();
 // create a  product
-router.post('/create-product', productControllers.createProduct);
+router.post('/', productControllers.createProduct);
 
 // get all product
 router.get('/', productControllers.getAllProducts);
